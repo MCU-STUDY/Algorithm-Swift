0a5da40043f0fa8f8d479c675b7984d441717247
kimscastle <uiskim22@pos.idserve.net>
2023-09-04 10:09:58 +0900

    📝 3주차 백트래킹 복습

--- 의성/알고리즘3주차/3week/3week/백준 1182 부분수열의 합.swift
+++ 의성/알고리즘3주차/3week/3week/백준 1182 부분수열의 합.swift
@@ -16,21 +16,41 @@ var visited = Array(repeating: false, count: 21)
 
 var cnt = 0
 var sum = 0
-func dfs(_ depth: Int, _ start: Int){
-    if sum == s && depth >= 1{
+func dfs(_ input: [Int], start: Int){
+    if input.count > 0, input.reduce(0, +) == s {
         cnt += 1
     }
-    
-    for i in start..<n{
+
+    /// N과M은 무조건 1부터 n까지 for문을 돌았기 때문에
+    /// [1,2,3,4]라면 늘 1,2,3,4로 돎
+    /// 시작점과 상관없이 1,2,3,4순으로 돌지만
+    /// 이 문제의 경우엔 -7, -3, -2, 5, 8에서 -3에서 출발할때는 -7을 배제해야하고 -2에서 출발할때는 -7과 -3을 배제해야함
+    /// 처음에는 []로 시작했다가
+    /// i에 0이들어가서 -7이 들어감, 여전히 i는 0
+    /// [-7]에 start는 0
+    /// [-7, -3]에 start는 1
+    /// [-7, -3, -2]에 start는 2
+    /// [-7, -3, -2, 5]에 start는 3
+    /// [-7, -3, -2, 5, 8]에 start는 4
+    /// ... -7이 시작점인 모든 경우의 수를 누적해서 출력함
+    ///
+    /// start가 0일때 재귀가 끝나서 start가 1이 됨
+    /// 그러면 [-3]이 들어가고 start는 1이됨
+    /// [-3, -2]에 start는 2
+    /// [-3, -2, 5]에 start는 3
+    /// [-3, -2, 5, 8]에 start는 4
+    /// ... -3이 시작점인 모든 경우의 수를 누적해서 출력함
+    ///
+    /// start가 1일때 재귀가 끝나서 start가 2가됨
+    /// [-2]가 들어가고 start는 2가 됨
+    for i in start..<arr.count {
         if !visited[i]{
             visited[i] = true
-            sum += arr[i]
-            dfs(depth + 1, i)
+            dfs(input + [arr[i]], start: i)
             visited[i] = false
-            sum -= arr[i]
         }
     }
 }
 
-dfs(0, 0)
+dfs([], start: 0)
 print(cnt)
--- 의성/알고리즘3주차/3week/3week/백준 15649 N과M(1).swift
+++ 의성/알고리즘3주차/3week/3week/백준 15649 N과M(1).swift
@@ -10,48 +10,34 @@ import Foundation
 /// 15649번
 /// N과 M(1)
 
-let input = readLine()!.split(separator: " ").map{Int(String($0))!}
-let lastNumber = input[0]
-let numberOfCount = input[1]
+/// isVisited사용
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+let n = input[0], m = input[1]
+var visited = [Bool](repeating: false, count: n + 1)
 
-var stack: [Int] = []
-
-func dfs() {
-    if stack.count == numberOfCount {
-        print(stack.map{String($0)}.joined(separator: " "))
+func dfs(nums: [Int]) {
+    /// 가지치기
+    if nums.count == m {
+        nums.forEach { print($0, terminator: " ") }
+        print()
         return
     }
     
-    for i in 1...lastNumber {
-        if !stack.contains(i) {
-            stack.append(i)
-            dfs()
-            stack.removeLast()
+    /// 이경우는 노드끼리의 value가 중복이 있는상태를 나타냄
+    /// 지금까지의 예제는 각 노드의 value가 unique했어서 visited가 같을때 다른 value가 없었는데
+    /// 숫자들의 list를 가지고 조합을 구할때는 왼쪽의 1과 오른쪽의 1은 다른 value로 봐야하기때문에 visited를 초기화 시켜줘야한다
+    ///
+    /// 예를들어서 input으로 [1,2]가 들어오더라도
+    /// [1,2,3,4]와 비교해서 1을 방문했고 2를방문했으니 다음에 3을 넣자!의 느낌임
+    for i in 1...n {
+        if !visited[i] {
+            visited[i] = true
+            dfs(nums: nums + [i])
+            /// 모든 경우의 수
+            /// 1번레벨 (empty) 2번레벨 (1) (2) (3) (4)
+            visited[i] = false
         }
     }
 }
 
-dfs()
-
-/// isVisited사용
-//let input = readLine()!.split(separator: " ").map { Int($0)! }
-//let n = input[0], m = input[1]
-//var visited = [Bool](repeating: false, count: n + 1)
-//
-//func dfs(nums: [Int]) {
-//    if nums.count == m {
-//        nums.forEach { print($0, terminator: " ") }
-//        print()
-//        return
-//    }
-//
-//    for i in 1...n {
-//        if !visited[i] {
-//            visited[i] = true
-//            dfs(nums: nums + [i])
-//            visited[i] = false
-//        }
-//    }
-//}
-//
-//dfs(nums: [])
+dfs(nums: [])
